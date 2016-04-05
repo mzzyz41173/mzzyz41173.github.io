1a735e0675469f74d54aa9a942cf27523f33acaa
mzzyz41173 <1207846425@qq.com>
2016-04-05 19:49:46 +0800

    新增上传脚本

--- go.sh
+++ go.sh
@@ -1,5 +1,6 @@
-git add .
 git config --global user.email "1207846425@qq.com"
 git config --global user.name "mzzyz41173"
+git config --global push.default simple
+git add .
 git commit -m $1
 git push
